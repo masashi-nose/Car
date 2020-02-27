b43fcbd664900e63fbab85a1fc300e77b5350244
nosemasashi <ma.nose9413@gmail.com>
2020-02-27 14:14:25 +0900

    Carクラスに機能を加えました。

--- src/main/java/com/example/demo/Car.java
+++ src/main/java/com/example/demo/Car.java
@@ -1,5 +1,34 @@
 package com.example.demo;
 
+/**
+ * Carクラスです.
+ * 
+ * @author masashi.nose
+ *
+ */
 public class Car {
+	private int speed;
+	private String color;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", color=" + color + "]";
+	}
 
 }
