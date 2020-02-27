a293daaf2cac6d0cd2d6575b2ecda44aad34c281
atsushi-nonaka <atsushi.nonaka@rakus-partners.co.jp>
2020-02-27 14:14:21 +0900

    Carクラスに2機能を加えました。

--- src/main/java/com/example/demo/Car.java
+++ src/main/java/com/example/demo/Car.java
@@ -1,5 +1,24 @@
 package com.example.demo;
 
 public class Car {
-
+	private int speed;
+	private String color;
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", color=" + color + "]";
+	}
+	
+	
 }
